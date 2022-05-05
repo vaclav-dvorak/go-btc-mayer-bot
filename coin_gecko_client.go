ec089d8c909829b3500d2df595b3efcacb188bfd
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2022-05-05 19:07:54 +0200

    feat: add possbile exchange rate

---
+++ coin_gecko_client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const days = 200
+
+type avgResponse struct {
+	Prices [][]float64 `json:"prices"`
+}
+
+type rateResponse struct {
+	Rate map[string]float64 `json:"tether"`
+}
+
+func getAvgPrice() (avg float64) {
+	var (
+		data avgResponse
+		cur  = conf.Currency
+	)
+
+	ctx := context.Background()
+	header := http.Header{
+		"Accept":       []string{"application/json"},
+		"Content-Type": []string{"application/json"},
+	}
+	req, _ := http.NewRequestWithContext(ctx, "GET", "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart", nil)
+	req.Header = header
+	q := req.URL.Query()
+	q.Add("days", strconv.Itoa(days-1))
+	q.Add("vs_currency", cur)
+	req.URL.RawQuery = q.Encode()
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Print(err)
+	}
+	_ = res.Body.Close()
+	sum := 0.0
+	for _, day := range data.Prices {
+		sum += day[1]
+	}
+	avg = sum / float64(days)
+	log.Printf("%d day moving avarage: %s%.2f%s(%s)\n", days, blue, avg, reset, cur)
+	return
+}
+
+func getConversionRate(cur string) (rate float64) {
+	var (
+		data rateResponse
+	)
+
+	ctx := context.Background()
+	header := http.Header{
+		"Accept":       []string{"application/json"},
+		"Content-Type": []string{"application/json"},
+	}
+	req, _ := http.NewRequestWithContext(ctx, "GET", "https://api.coingecko.com/api/v3/simple/price", nil)
+	req.Header = header
+	q := req.URL.Query()
+	q.Add("ids", "tether")
+	q.Add("vs_currencies", cur)
+	req.URL.RawQuery = q.Encode()
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	body, _ := ioutil.ReadAll(res.Body)
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Print(err)
+	}
+	_ = res.Body.Close()
+	rate = data.Rate[cur]
+	log.Printf("exchange rate for %s%s%s: %s%.4f%s\n", green, cur, reset, blue, data.Rate[cur], reset)
+	return
+}
--- main.go
+++ main.go
@@ -11,7 +11,13 @@ import (
 )
 
 type config struct {
-	Currency string `koanf:"currency"`
+	Currency  string `koanf:"currency"`
+	CCurrency string `koanf:"convert_currency"`
+	Orders    []struct {
+		Mayer  float64 `koanf:"mayer"`
+		Volume float64 `koanf:"volume"`
+	} `koanf:"orders"`
+	CancelOrders bool `koanf:"cancel_current_orders"`
 }
 
 var (
@@ -27,10 +33,30 @@ func main() {
 	if err := k.Unmarshal("", &conf); err != nil {
 		log.Fatalf("error parsing config: %v", err)
 	}
+	vol := 0.0
+	for _, order := range conf.Orders {
+		vol += order.Volume
+	}
+	if vol > 1 {
+		log.Fatalf("sum of all order volumes must be equal or less then 1. yours is %.2f", vol)
+	}
 	// telegramToken = os.Getenv("TELEGRAM_APITOKEN")
 
 	printWelcome()
 	avg := getAvgPrice()
-	fmt.Printf("0.8 * avg = %.2f in eur (%.2f)\n", avg*0.8, avg*0.8*0.94)
-	fmt.Printf("0.79 * avg = %.2f in eur (%.2f)\n", avg*0.79, avg*0.79*0.94)
+	rate := 1.0
+	if conf.Currency == "usd" && conf.CCurrency != "" {
+		rate = getConversionRate(conf.CCurrency)
+	}
+
+	for _, order := range conf.Orders {
+		target := order.Mayer
+		msg := fmt.Sprintf("%.2f target = %s%.2f%s(%s%s%s)", target, blue, avg*target, reset, green, conf.Currency, reset)
+		if rate != 1.0 {
+			msg += fmt.Sprintf(" or converted %s%.2f%s(%s%s%s)\n", blue, avg*target*rate, reset, green, conf.CCurrency, reset)
+		} else {
+			msg += "\n"
+		}
+		log.Print(msg)
+	}
 }
